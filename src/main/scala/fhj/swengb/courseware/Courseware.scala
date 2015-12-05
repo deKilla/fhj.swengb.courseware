39ce0f897ba412811baf7836d88df3d21c774175
carinaher <carina.herzog@gmail.com>
2015-12-05 12:15:43 +0100

    added file-structure

---
+++ src/main/scala/fhj/swengb/courseware/Courseware.scala
@@ -0,0 +1,11 @@
+package fhj.swengb.courseware
+
+
+//object launches the class described beneath
+object Courseware {
+
+  def main(args: Array[String]) {
+    println("hello world")
+  }
+
+}
---
+++ src/test/scala/fhj/swengb/courseware/CoursewareTest.scala
@@ -0,0 +1,8 @@
+package fhj.swengb.courseware
+
+/**
+  * Created by carin on 05/12/2015.
+  */
+class CoursewareTest {
+
+}
