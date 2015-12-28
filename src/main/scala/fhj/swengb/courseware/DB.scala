fca9a47983d0dbddb5b2ea340437c0aab3bb5bfb
Michael Fuchs <admin@dekilla.at>
2015-12-28 17:10:03 +0100

    added depedency and absolute path (for now)

--- src/main/scala/fhj/swengb/courseware/DB.scala
+++ src/main/scala/fhj/swengb/courseware/DB.scala
@@ -4,30 +4,24 @@ import java.sql.{Connection,DriverManager,ResultSet}
 import scala.util.Try
 
 object DB {
-
   trait DBEntity[T] {
     def fromDB(rs: ResultSet): List[T]
-
-    def query(c: Connection)(q: String): ResultSet = {
-      c.createStatement().executeQuery(q)
-    }
+    def query(c: Connection)(q: String): ResultSet = c.createStatement().executeQuery(q)
   }
-
-  lazy val maybeConnection: Try[Connection] =
-    Try(DriverManager.getConnection("jdbc:sqlite:exampledb.sqlite"))
-
+  lazy val maybeConnection: Try[Connection] = Try(DriverManager.getConnection("jdbc:sqlite:E:/SWENGB/fhj.swengb.courseware/src/main/scala/fhj/swengb/courseware/exampledb.sqlite"))
 }
 
 
 object DBAccess {
-
   def main(args: Array[String]) {
-    for {con <- DB.maybeConnection
-         content <- Album.fromDB(Album.showeverything(con))
+    val connection = DB.maybeConnection
+    if (connection.isSuccess) println("connection established")
+    for {c <- connection
+         row <- Album.fromDB(Album.showeverything(c))
     } {
       println("printing results:")
       println("-------------------------------------------------")
-      println(content)
+      println(row)
       println("-------------------------------------------------")
       println("results printed")
     }
